2d5655b35ff09675acfd05d39be7da3f6c815b2f
legato-desu <ivanchokr@gmail.com>
2023-01-16 06:28:00 -0500

    Ejercicios

--- 21_tabla_multi.py
+++ 21_tabla_multi.py
@@ -4,10 +4,9 @@
 Escribir un programa en el cual se solicite al usuario un número y 
 se imprima la tabla de multiplicar del 1 al 10 del valor introducido.
 """
-vacia = ""
-print(vacia.center(">>> TABLA DE MULTIPLICAR<<<"))
+print(">>> TABLA DE MULTIPLICAR<<<")
 numero = int(input('>>> Ingrese el numero a multiplicar: '))
  
 for multiplicador in range(1,11):
     resultado = numero * multiplicador
-    print("%d x %2d = %d" % (numero,multiplicador,resultado))
+    print(numero, " x ", multiplicador, " = ",resultado)
